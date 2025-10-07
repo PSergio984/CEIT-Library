2910ccdfe5ead605f38298454895a149e7ffd68b
Bryan Dela Cruz <137765730+Bryan652@users.noreply.github.com>
2025-10-06 22:26:42 -0400

    Filter and Search
    - Addition of Title, Authors, Paper Type, publication year of the paper borrowed

--- app/Livewire/Pages/admin/AdminBorrowTransactions.php
+++ app/Livewire/Pages/admin/AdminBorrowTransactions.php
@@ -2,21 +2,95 @@
 
 namespace App\Livewire\Pages\Admin;
 
+use App\Models\AcademicPaper;
 use App\Models\BorrowTransaction;
 use Livewire\Component;
+use Livewire\WithPagination;
 
-class AdminBorrowTransactions extends Component
+class AdminBorrowTransactions extends AdminComponent
 {
+    use WithPagination;
+
+    public $perPage = 15;
+    public $search = '';
+    public $paperTypeFilter = '';
+    public $yearFrom = '';
+    public $yearTo = '';
+
+    protected function getTransactionsQuery()
+    {
+        return BorrowTransaction::with([
+            'user',
+            'inventory.academicPaper.authors'  // Get book details
+        ])
+        ->when($this->search, function($query) {
+            $query->whereHas('user', function($q) {
+                $q->where('first_name', 'like', "%{$this->search}%")
+                  ->orWhere('last_name', 'like', "%{$this->search}%")
+                  ->orWhere('email', 'like', "%{$this->search}%")
+                  ->orWhere('student_no', 'like', "%{$this->search}%");
+            })
+            ->orWhereHas('inventory.academicPaper', function($q) {
+                $q->where('title', 'like', "%{$this->search}%");
+            });
+        })
+        ->when($this->paperTypeFilter, function($query) {
+            $query->whereHas('inventory.academicPaper', function($q) {
+                $q->where('paper_type', $this->paperTypeFilter);
+            });
+        })
+        ->when($this->yearFrom, function($query) {
+            $query->whereHas('inventory.academicPaper', function($q) {
+                $q->where('publication_year', '>=', $this->yearFrom);
+            });
+        })
+        ->when($this->yearTo, function($query) {
+            $query->whereHas('inventory.academicPaper', function($q) {
+                $q->where('publication_year', '<=', $this->yearTo);
+            });
+        })
+        ->orderBy('time_in', 'desc');
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function updatingPaperTypeFilter()
+    {
+        $this->resetPage();
+    }
+
+    public function updatingYearFrom()
+    {
+        $this->resetPage();
+    }
+
+    public function updatingYearTo()
+    {
+        $this->resetPage();
+    }
+
+    public function clearFilters()
+    {
+        $this->search = '';
+        $this->paperTypeFilter = '';
+        $this->yearFrom = '';
+        $this->yearTo = '';
+        $this->resetPage();
+    }
+
     public function render()
     {
-        $transactions = BorrowTransaction::with(['user' => function($query) {
-            $query->select('id', 'first_name', 'last_name', 'email', 'student_no');
-        }])
-        ->orderBy('time_in', 'desc')
-        ->paginate(20);
+        $transactions = $this->getTransactionsQuery()->paginate($this->perPage);
+
+        // Get unique paper types for filter dropdown
+        $paperTypes = AcademicPaper::distinct()->pluck('paper_type')->filter();
 
         return view('livewire.pages.admin.admin-borrow-transactions', [
-            'transactions' => $transactions
+            'transactions' => $transactions,
+            'paperTypes' => $paperTypes
         ]);
     }
 }
