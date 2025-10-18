94e53c6772599130a8f28d7285ed70d37e275012
PSergio984 <ericgabrielmanabat@plv.edu.ph>
2025-10-18 22:02:42 +0800

    fix: correct namespace casing and add Title attribute to AdminDashboard component

--- app/Livewire/Pages/admin/AdminComponent.php
+++ app/Livewire/Pages/admin/AdminComponent.php
@@ -2,18 +2,12 @@
 
 namespace App\Livewire\Pages\Admin;
 
-
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 #[Layout('components.layouts.Admin')]
 class AdminComponent extends Component
 {
-
-    public function render()
-    {
-        return view('livewire.admin-dashboard');
-    }
+    // Base component for all admin pages
+    // Layout is defined via the #[Layout] attribute above
 }
-
-
