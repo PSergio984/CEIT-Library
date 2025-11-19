4315b9c637f49eb7648788e74a830db8cffb633a
Harayku <jetangelo04.jp@gmail.com>
2025-11-20 04:16:41 +0800

    feat: implement PDF export functionality for attendance logs and add corresponding view

--- app/Livewire/Pages/admin/AdminAttendanceLogIndex.php
+++ app/Livewire/Pages/admin/AdminAttendanceLogIndex.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Pages\admin;
 
 use App\Models\Attendance;
 use App\Models\Role;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Livewire\Attributes\Lazy;
 use Livewire\Attributes\Title;
 use Livewire\WithPagination;
@@ -194,7 +195,25 @@ class AdminAttendanceLogIndex extends AdminComponent
         // Get all attendances matching current filters (no pagination)
         $query = $this->getAttendancesQuery();
 
-        $query = $this->applySorting($query);
+        if (isset($this->sortBy['column']) && isset($this->sortBy['direction'])) {
+            $column = $this->sortBy['column'];
+            $direction = $this->sortBy['direction'];
+
+            if ($column === 'user_name') {
+                $query->join('users', 'attendances.user_id', '=', 'users.id')
+                    ->orderBy('users.first_name', $direction)
+                    ->select('attendances.*');
+            } elseif ($column === 'scanned_by_name') {
+                $query->leftJoin('librarians', 'attendances.scanned_by', '=', 'librarians.id')
+                    ->leftJoin('users as librarian_users', 'librarians.user_id', '=', 'librarian_users.id')
+                    ->orderBy('librarian_users.first_name', $direction)
+                    ->select('attendances.*');
+            } else {
+                $query->orderBy($column, $direction);
+            }
+        } else {
+            $query->orderBy('time_in', 'desc');
+        }
 
         $attendances = $query->get()->map(function ($attendance) {
             return [
