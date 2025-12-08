14fa9285f7c002ff8077b9373d4fce7dc2996a66
Harayku <jetangelo04.jp@gmail.com>
2025-12-09 03:27:42 +0800

    Add Vite image import for asset processing in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,11 @@ import "./bootstrap";
 import { Html5Qrcode } from "html5-qrcode";
 import jsQR from "jsqr";
 
+// Import images for Vite processing
+import.meta.glob([
+    '../images/**',
+]);
+
 // Make Html5Qrcode available globally
 window.Html5Qrcode = Html5Qrcode;
 // Make jsQR available globally for file-based QR scanning
